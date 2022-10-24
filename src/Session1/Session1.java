5254c53444450b5c211e4df207c817e22f1e7bcd
Islam AlaaEddin <islamalaaeddin1998@gmail.com>
2022-10-24 18:38:41 +0200

    Session 1 - Modeling and Code

--- src/Session1/Session1.java
+++ src/Session1/Session1.java
@@ -2,7 +2,18 @@ package Session1;
 
 public class Session1 {
     public static void main(String[] args) {
-        System.out.println("Initial Commit");
+        System.out.println("ParallelogramArea: " + getParallelogramArea(10f, 6f));
+        System.out.println("TrapezoidArea: " + getTrapezoidArea(10f, 6f, 2f));
+    }
+
+    private static float getParallelogramArea(float base, float height) {
+        float area = base * height;
+        return area;
+    }
+
+    private static float getTrapezoidArea(float upperBase, float loweBase, float height) {
+        float area = ((upperBase + loweBase) / 2) * height;
+        return area;
     }
 }
 
